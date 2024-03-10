2fc89bb11a68658b8383b0ab2e084262d3b619ad
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-03-10 15:39:15 -0700

    Using Express route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,19 +13,7 @@ const tours = JSON.parse(
   readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf8'),
 ) as any[];
 
-app.get('/api/v1/tours', (req: Request, res: Response) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-});
-
-//localhost:3000/api/v1/tours/5/2/3
-//add ? to make a Path parameter optional '/api/v1/tours/:id/:x/:y?'
-app.get('/api/v1/tours/:id', (req: Request, res: Response): any => {
+const getTour = (req: Request, res: Response): any => {
   console.log(req.params);
   const id = parseInt(req.params.id); //convert to int
   const tour = tours.find((el) => el.id === id);
@@ -43,9 +31,9 @@ app.get('/api/v1/tours/:id', (req: Request, res: Response): any => {
       tour,
     },
   });
-});
+};
 
-app.patch('/api/v1/tours/:id', (req: Request, res: Response): any => {
+const patchTour = (req: Request, res: Response): any => {
   const id = parseInt(req.params.id); //convert to int
   if (id > tours.length) {
     return res.status(404).json({
@@ -61,9 +49,25 @@ app.patch('/api/v1/tours/:id', (req: Request, res: Response): any => {
       tour: '<Updated tour here...>',
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req: Request, res: Response) => {
+const deleteTour = (req: Request, res: Response): any => {
+  const id = parseInt(req.params.id); //convert to int
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      data: {
+        tour: 'Invalid Id',
+      },
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+const postTour = (req: Request, res: Response) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -82,7 +86,28 @@ app.post('/api/v1/tours', (req: Request, res: Response) => {
       });
     },
   );
-});
+};
+
+const getAllTours = (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+//localhost:3000/api/v1/tours/5/2/3
+//add ? to make a Path parameter optional '/api/v1/tours/:id/:x/:y?'
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', patchTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+// app.post('/api/v1/tours', postTour);
+// app.get('/api/v1/tours', getTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(postTour);
+app.route('/api/v1/tours/:id').get(getTour).patch(patchTour).delete(deleteTour);
 
 app.listen(port, () => {
   console.log(
