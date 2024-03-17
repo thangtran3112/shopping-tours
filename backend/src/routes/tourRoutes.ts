5bc448636805eefdb4a80a5c1c57fa45ff0a9030
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-03-16 20:53:41 -0700

    Unwind and Project Aggregation Stages

--- backend/src/routes/tourRoutes.ts
+++ backend/src/routes/tourRoutes.ts
@@ -6,6 +6,7 @@ import {
   updateTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } from '../controllers/tourController';
 import express from 'express';
 
@@ -13,6 +14,7 @@ const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 // router.param('id', checkID);
 
