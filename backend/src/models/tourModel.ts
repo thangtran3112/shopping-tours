229f30c178ec0dc15e36ca2225be17bee8896db3
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-03-17 09:28:22 -0700

    add Virtual properties to JSON output

--- backend/src/models/tourModel.ts
+++ backend/src/models/tourModel.ts
@@ -23,72 +23,84 @@ export interface ITour {
   startDates?: Date[] | string[]; //Mongoose will convert it to Date
 }
 
-const tourSchema = new mongoose.Schema<ITour>({
-  name: {
-    type: String, //native Js types
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-    enum: {
-      values: Object.values(Difficulty),
-      message: `Difficulty must be either: ${Object.values(Difficulty)}`,
+const tourSchema = new mongoose.Schema<ITour>(
+  {
+    name: {
+      type: String, //native Js types
+      required: [true, 'A tour must have a name'],
+      unique: true,
     },
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingQuantity: {
-    type: Number,
-    default: 0,
-  },
-  discount: {
-    type: Number,
-    validate: {
-      validator: function (this: ITour, value: number) {
-        return value < this.price;
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: Object.values(Difficulty),
+        message: `Difficulty must be either: ${Object.values(Difficulty)}`,
       },
-      message: 'Discount price ({VALUE}) should be below regular price',
     },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0,
+    },
+    discount: {
+      type: Number,
+      validate: {
+        validator: function (this: ITour, value: number) {
+          return value < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price',
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    //if input is a string, it will be converted to Date
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      // select: false, //exclude this field from Database response projection
+    },
+    //if input is a string array, it will be converted to Date array
+    startDates: [Date],
   },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a summary'],
+  {
+    //each time the schema is converted to JSON, virtual properties will be added
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  //if input is a string, it will be converted to Date
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    // select: false, //exclude this field from Database response projection
-  },
-  //if input is a string array, it will be converted to Date array
-  startDates: [Date],
+);
+
+//virtual property, which is available on the document but not in the database
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 //model is uppercase
