a9923acede3b9cf2834f587f8142efa2771e8eb6
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-04-19 21:40:38 -0700

    Global error handling for prod and dev

--- backend/src/controllers/errorController.ts
+++ backend/src/controllers/errorController.ts
@@ -1,6 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response, NextFunction } from 'express';
 
+const sendErrorDev = (err: any, res: Response) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err: any, res: Response) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR 💥', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 export const globalErrorHandler = (
   err: any,
   req: Request,
@@ -12,6 +37,12 @@ export const globalErrorHandler = (
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
