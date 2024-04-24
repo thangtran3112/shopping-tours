70b0c769e6fe42a1c322cdd5bfec02bbd14f2c29
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-04-23 22:16:23 -0700

    Route for update current user password

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import {
   forgotPassword,
   login,
+  protect,
   resetPassword,
   signup,
+  updatePassword,
 } from '../controllers/authController';
 import {
   getAllUsers,
@@ -19,6 +21,7 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
+router.patch('/updateMyPassword', protect, updatePassword);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
--- backend/src/utils/catchAsync.ts
+++ backend/src/utils/catchAsync.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
+import { AppRequest } from './types';
 
 export const catchAsync = (
-  fn: (req: Request, res: Response, next: NextFunction) => any,
+  fn: (req: Request | AppRequest, res: Response, next: NextFunction) => any,
 ) => {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return (req: Request | AppRequest, res: Response, next: NextFunction) => {
     fn(req, res, next).catch(next);
   };
 };
